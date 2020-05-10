918fce08a97859fbbd5598bd5e3a4167c747f487
Den <dnseroshtanov@hotmail.com>
2020-05-10 12:22:18 +0300

    Initial commit

---
+++ NetBox.py
@@ -0,0 +1,14 @@
+import requests
+
+url = "https://lgb-netbox01.int/api/dcim/interfaces?device=mia-wvrtcore-a"
+
+payload = {}
+headers = {
+  'Accept': 'application/json',
+  'Authorization': 'Token NetBox Token'
+}
+
+response = requests.request("GET", url, headers=headers, data = payload)
+
+print(response.text.encode('utf8'))
+print('test')
